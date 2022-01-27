bbe0e35c5f145f3395b68eaa4767c28098f854be
Frank Fleurt <frank.fleurt@hotmail.com>
2022-01-27 09:20:07 +0100

    end header save

--- src/assets/js/Nav.js
+++ src/assets/js/Nav.js
@@ -1,6 +1,13 @@
-const nav = document.querySelector('nav')
+const nav = document.querySelector('.navContainer')
 const navBurger = document.querySelector(".navburger")
+const close = document.querySelector("#close")
 
 menuBurger.addEventListener("click", () => {
-  nav.classList.toggle("navburger");
+  nav.style.animation = "navAnimeIn 0.5s forwards"
+  menuBurger.style.zIndex = "0"
+})
+close.addEventListener("click", () => {
+  console.log("test");
+  nav.style.animation = "navAnimeOut 0.5s forwards"
+  menuBurger.style.zIndex = "151"
 })
